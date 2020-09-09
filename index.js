0d3d01668ef540fd0dd0e03675e3b55092f8a898
Robert Shilcof <robert.shilcof@gmail.com>
2020-09-09 09:58:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  var del = cart.reduce((a, b) =>  ((b.itemPrice === item) ? a + b : a), -1);
+  var del = cart.reduce((a, b) => ((b.itemPrice === item) ? a + b : a), -1);
   if (del !== -1) {
     return cart.splice(del+1,1)
   } else {
